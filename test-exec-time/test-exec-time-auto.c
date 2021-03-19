f067d0fcd31bdd049a16df709ff590d0c05ee27f
Infernus <007yary.ru@gmail.com>
2021-03-19 22:26:49 +0300

    Добавлены программы, выводящие время выполнения цикла при использовании переменной с классом хранения auto и register (по отдельности)

---
+++ test-exec-time/test-exec-time-auto.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+  long a = 0;
+  clock_t start, end;
+  double exec_time;
+
+  start = clock();
+  for (int i = 0; i < 1000000; i++) {
+    a += i * i + 5;
+  }
+  end = clock();
+
+  exec_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("Программа выполнилась за %lf секунд\n", exec_time);
+
+  exit(EXIT_SUCCESS);
+}
+  
