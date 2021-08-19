769564a540882a56d2f148121b148567b3a818cd
TotalOverDose <007yary.ru@gmail.com>
2021-08-19 20:12:53 +0300

    An inet family loopback client-server app using a stream type socket

--- networks/sockets/inet/client.c
+++ networks/sockets/inet/client.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h> // strcpy
 #include <stdlib.h> // malloc
+#include <unistd.h> // close
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -15,11 +16,15 @@ int main(void)
 	int retval = -1;
 	int socket_id = -1;
 
-	socket(AF_INET, SOCK_DGRAM, 0);
+	socket_id = socket(AF_INET, SOCK_STREAM, 0);
 	if (-1 == socket_id)
 	{
 		perror("Create a socket");
 	}
+
+	server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = SERVER_PORT;
 
 	strcpy(message, "Hello!");
 
@@ -27,12 +32,30 @@ int main(void)
 			sizeof(struct sockaddr_in));
 	if (-1 == retval)
 	{
-		perror("Connect to a server");
+		perror("Connect to the server");
 	}
 
 	retval = send(socket_id, message, MSG_SIZE, 0);
 	if (-1 == retval)
 	{
-		perror("Send a message to a server");
+		perror("Send a message to the server");
 	}
+	else
+	{
+		printf("A message was sent: %s\n", message);
+	}
+
+	retval = recv(socket_id, message, MSG_SIZE, 0);
+	if (-1 == retval)
+	{
+		perror("Receive a message from the server");
+	}
+	else
+	{
+		printf("A message was received: %s\n", message);
+	}
+
+	close(socket_id);
+
+	free(message);
 }
--- networks/sockets/inet/server.c
+++ networks/sockets/inet/server.c
@@ -1,6 +1,81 @@
+#include "../interface.h"
 
+#include <stdio.h>
+#include <unistd.h> // close
+#include <stdlib.h> // malloc, free
+#include <string.h> // strcpy
+
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(void)
 {
+	int server_sock_id = -1;
+	int client_sock_id = -1;
+	int retval = -1;
+	socklen_t socklen = sizeof(struct sockaddr_in);
 
+	char *msg_buf = malloc(MSG_SIZE);
+
+	struct sockaddr_in server_addr, client_addr;
+
+	server_sock_id = socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == server_sock_id)
+	{
+		perror("Create a socket");
+	}
+
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = SERVER_PORT;
+
+	retval = bind(server_sock_id, (struct sockaddr*) &server_addr, socklen);
+	if (-1 == retval)
+	{
+		perror("Bind a server name");
+	}
+
+	retval = listen(server_sock_id, 5);
+	if (-1 == retval)
+	{
+		perror("Listen from clients");
+	}
+	else
+	{
+		printf("Waiting for clients' connection requests...\n");
+	}
+
+	client_sock_id = accept(server_sock_id, (struct sockaddr*) &client_addr,
+			&socklen);
+	if (-1 == client_sock_id)
+	{
+		perror("Accept a client");
+	}
+	else
+	{
+		printf("The connection request was accepted\n");
+	}
+
+	retval = recv(client_sock_id, msg_buf, MSG_SIZE, 0);
+	if (-1 == retval)
+	{
+		perror("Receive a message form a client");
+	}
+	else
+	{
+		printf("Received a message: %s\n", msg_buf);
+
+		strcpy(msg_buf, &msg_buf[1]);
+
+		retval = send(client_sock_id, msg_buf, MSG_SIZE, 0);
+		if (-1 == retval)
+		{
+			perror("Send a message to a client");
+		}
+	}
+
+	close(server_sock_id);
+	close(client_sock_id);
+
+	free(msg_buf);
 }
